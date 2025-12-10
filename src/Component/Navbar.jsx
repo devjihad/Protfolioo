f75322bc22128a9b92f9b574d59d8976589c9c6d
devjihad <mdhajihad609@gmail.com>
2025-12-10 23:42:07 +0600

    demo

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
-
+import logo from '../assets/logo-2418159_1280-removebg-preview.png'
 const Navbar = () => {
     return (
        <div className=" bg-gray-100 text-gray-900 font-sans">
                {/* Navbar */}
                <nav className="w-full bg-white shadow ">
                  <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
-                   <h1 className="text-2xl font-bold text-blue-600">MyPortfolio</h1>
+                  <img className='w-[70px] h-[70px]' src={logo} alt="" />
                    <ul className="flex gap-6 text-lg">
                      <Link to="/" className="hover:text-blue-600">Home</Link>
                      <Link to="/about" className="hover:text-blue-600">About</Link>
