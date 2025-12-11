3a2ef19a0c84e3c95d958fbe4f29bb072b0b441c
devjihad <mdhajihad609@gmail.com>
2025-12-11 22:24:28 +0600

    done

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,6 +1,29 @@
 import { Link } from "react-router";
 import me from '../assets/downlsdfoad).png'
+import letsfood from '../assets/letsFood.png'
+import tomato from '../assets/tomato.png'
+import relive from '../assets/relive.png'
 function Home() {
+  const project =[
+    {
+      image:letsfood,
+      title:'Lets Food',
+      description:'A simple food web page using Javascript',
+      link:'https://devjihad.github.io/food-kit/'
+    },
+    {
+      image:relive,
+      title:'Relive Hospital',
+      description:'A simple Hospital web page using React Js & Tailwind css',
+      link:'https://relive-frontend-steel.vercel.app/'
+    },
+    {
+      image:tomato,
+      title:'Resturant web page',
+      description:'Resturant web page using React Js & Tailwind css',
+      link:'https://food-app-iota-umber-55.vercel.app/'
+    },
+  ]
   return (
     <div className="max-w-6xl mx-auto p-4 space-y-20">
       
@@ -24,7 +47,7 @@ function Home() {
       <section id="skills" className="space-y-6">
         <h2 className="text-3xl font-bold">My Skills</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6 ">
-          {["React.js", "JavaScript", "Tailwind CSS", "HTML & CSS", "Git", "Node.js", "Firebase", "API Integration"].map((skill) => (
+          {["React.js", "JavaScript", "Tailwind CSS", "HTML & CSS", "Git", "Firebase", "API Integration"].map((skill) => (
             <div key={skill} className="p-4 rounded-xl shadow text-center font-semibold hover:scale-105 transition bg-stone-400">
               {skill}
             </div>
@@ -36,11 +59,13 @@ function Home() {
       <section id="projects" className="space-y-6">
         <h2 className="text-3xl font-bold">Featured Projects</h2>
         <div className="grid md:grid-cols-3 gap-6">
-          {[1,2,3].map((n) => (
+          {project.map((n) => (
             <div key={n} className="bg-white rounded-xl shadow p-4 hover:-translate-y-2 transition">
-              <div className="w-full h-40 bg-gray-200 rounded-lg mb-4"></div>
-              <h3 className="font-semibold text-xl mb-2">Project {n}</h3>
-              <p className="text-gray-600">A modern project built using React.js and Tailwind CSS.</p>
+              <div className="w-full h-40 bg-gray-200 rounded-lg mb-4">
+                <Link to={n.link}><img className="w-full h-full rounded-xl" src={n.image} alt="" /></Link>
+              </div>
+              <h3 className="font-semibold text-xl mb-2">{n.title}</h3>
+              <p className="text-gray-600">{n.description}</p>
             </div>
           ))}
         </div>
